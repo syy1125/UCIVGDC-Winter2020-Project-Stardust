8ead69adfbebdbb25a5c12121858bb47c93b57ba
syy1125 <syy1125@hotmail.com>
2020-01-17 18:00:30 -0800

    Unify units

---
+++ Assets/Scripts/Testing/Clock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Clock : MonoBehaviour
+{
+	private Text _text;
+
+	private void Start()
+	{
+		_text = GetComponent<Text>();
+	}
+
+	private void Update()
+	{
+		_text.text = Time.time.ToString("00.000");
+	}
+}
