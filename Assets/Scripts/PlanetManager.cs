491a1b38314e51124f8caef339ac21d4a34e2033
bryanly200 <bryanly200@gmail.com>
2020-01-28 14:32:20 -0800

    Planets now show up in their orbits
    Planets show up but they don't move or anything yet. Also their sizes are not up to scale

---
+++ Assets/Scripts/PlanetManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+    public CelestialBody Body;
+
+    private void Start()
+    {
+        if (Body == null) return;
+
+        transform.position = Body.GetGlobalPositionAndVelocityAt(0).Item1;
+    }
+}
