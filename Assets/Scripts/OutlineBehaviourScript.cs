b691fc009d418fb901ea947b9ec46b913e36d99b
JC <hcha3@uci.edu>
2020-01-29 20:16:53 -0800

    Planet List.
    Added UI element outlining all planets.
    manually set the array for the moment.
    How do I access object color?

---
+++ Assets/Scripts/OutlineBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutlineBehaviourScript : MonoBehaviour
+{
+    public static int objNum = 9;
+    public int offsetSize = 30;   
+    public GameObject lineUnit;
+
+    public Object[] objList = new Object[objNum];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       for (int i = 0; i < objNum; i++)
+        {
+                        
+            GameObject newObj = Instantiate(lineUnit, new Vector3 (transform.position.x, transform.position.y + offsetSize*-i, transform.position.z), transform.rotation);
+            newObj.GetComponentInChildren<Text>().text = objList[i].name;
+
+            //how to get color?....
+            //newObj.GetComponentInChildren<Image>().color = objList[i]. 
+            
+            newObj.transform.SetParent(this.transform.parent);
+
+        }
+                      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
