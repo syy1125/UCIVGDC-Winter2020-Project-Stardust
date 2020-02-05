b544ab8d2095a30d2676e4d5cd9470883c5e6037
syy1125 <syy1125@hotmail.com>
2020-02-04 17:58:39 -0800

    Implement ScriptableObject based event system

---
+++ Assets/Scripts/Event/EventBus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Event Bus", fileName = "Event")]
+public class EventBus : ScriptableObject
+{
+	private Action _event;
+
+	public void Invoke()
+	{
+		_event?.Invoke();
+	}
+
+	public void AddListener(Action callback)
+	{
+		_event += callback;
+	}
+
+	public void RemoveListener(Action callback)
+	{
+		_event -= callback;
+	}
+
+	private void OnDestroy()
+	{
+		_event = null;
+	}
+}
---
+++ Assets/Scripts/Event/EventListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventListener : MonoBehaviour
+{
+	public EventBus EventBus;
+	public bool ActiveWhenDisabled = true;
+	public UnityEvent OnEvent = new UnityEvent(); 
+
+	private void Awake()
+	{
+		EventBus.AddListener(EventHandler);
+	}
+
+	private void EventHandler()
+	{
+		if (enabled || ActiveWhenDisabled)
+		{
+			OnEvent.Invoke();
+		}
+	}
+
+	private void OnDestroy()
+	{
+		EventBus.RemoveListener(EventHandler);
+	}
+}
