ecc941f9cf3cea48b7f57bb4aee9ff53d413c4e9
syy1125 <syy1125@hotmail.com>
2020-02-13 18:03:48 -0800

    Make building scriptable objects

---
+++ Assets/Scripts/Planet/Building.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct BuildingTile
+{
+	public Sprite Sprite;
+	public Vector3 Offset;
+}
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Building", fileName = "Building")]
+public class Building : ScriptableObject
+{
+	public BuildingTile[] Tiles;
+}
