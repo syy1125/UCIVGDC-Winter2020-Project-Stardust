8cde8674f2169af8d2dbaa3c1d133056692ad2d7
syy1125 <syy1125@hotmail.com>
2020-02-15 21:20:39 -0800

    Implement building selection

--- Assets/Scripts/BuildingSelectionButtonUI.cs
+++ Assets/Scripts/BuildingSelectionButtonUI.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class BuildingSelectionButtonUI : MonoBehaviour
 {
 	[HideInInspector]
@@ -8,6 +11,40 @@ public class BuildingSelectionButtonUI : MonoBehaviour
 	public int Index;
 	[HideInInspector]
 	public BuildingTemplate Building;
+
+	[Header("References")]
+	public Text BuildingName;
+
+	[Header("Config")]
+	public Color SelectedColor;
+
+	private ColorBlock _initialColors;
+
+	private void Start()
+	{
+		BuildingName.text = Building.DisplayName;
+		_initialColors = GetComponent<Button>().colors;
+	}
+
+	public void SetSelected(bool selected)
+	{
+		var button = GetComponent<Button>();
+
+		if (selected)
+		{
+			ColorBlock colors = button.colors;
+			colors.disabledColor *= SelectedColor;
+			colors.highlightedColor *= SelectedColor;
+			colors.normalColor *= SelectedColor;
+			colors.pressedColor *= SelectedColor;
+			colors.selectedColor *= SelectedColor;
+			button.colors = colors;
+		}
+		else
+		{
+			button.colors = _initialColors;
+		}
+	}
 
 	public void OnClick()
 	{
--- Assets/Scripts/BuildingSelectionUI.cs
+++ Assets/Scripts/BuildingSelectionUI.cs
@@ -1,14 +1,18 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingSelectionUI : MonoBehaviour
 {
+	[Header("References")]
 	public BuildingTemplate[] Buildings;
 	public GameObject BuildingButtonPrefab;
+	public GameObject ExtraButtons;
+	public Button CancelButton;
 
 	private int _selectedIndex;
 	private const int UNSELECT_INDEX = -1; // Index for when nothing is selected
-	
+
 	public void LoadBuildings()
 	{
 		_selectedIndex = UNSELECT_INDEX;
@@ -17,17 +21,31 @@ public class BuildingSelectionUI : MonoBehaviour
 		{
 			GameObject button = Instantiate(BuildingButtonPrefab, transform);
 			var buttonController = button.GetComponent<BuildingSelectionButtonUI>();
-			
+
 			buttonController.BuildingSelection = this;
 			buttonController.Index = index;
 			buttonController.Building = Buildings[index];
 		}
+		
+		ExtraButtons.SetActive(true);
 	}
 
 	public void SetSelectedIndex(int index)
 	{
-		Debug.Log($"Selecting index {index}");
 		_selectedIndex = index;
+
+		foreach (Transform child in transform)
+		{
+			var button = child.GetComponent<BuildingSelectionButtonUI>();
+			button.SetSelected(button.Index == _selectedIndex);
+		}
+
+		CancelButton.interactable = index != UNSELECT_INDEX;
+	}
+
+	public void CancelSelection()
+	{
+		SetSelectedIndex(UNSELECT_INDEX);
 	}
 
 	public void UnloadBuildings()
@@ -36,5 +54,7 @@ public class BuildingSelectionUI : MonoBehaviour
 		{
 			Destroy(child.gameObject);
 		}
+		
+		ExtraButtons.SetActive(false);
 	}
 }
